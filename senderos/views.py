3e547788f4b3fd9cb6294f936a8e62908d3410f4
José Antonio Córdoba Gómez <joseinn@correo.ugr.es>
2021-06-17 16:43:06 +0200

    #8 editar un sendero

--- senderos/views.py
+++ senderos/views.py
@@ -46,3 +46,42 @@ def createSendero(request):
 		
 	return render(request, 'home_cards_senderos.html', context)
 
+def editar(request, id):
+	# @todo Mejorar el paso de fotos, si no hubiera fotos, 
+	# intentaria acceder al indice cero, qeu lo mismo ni existe
+	context = {}
+	context['id']=id
+	form = SenderoForm()
+	sendero = Sendero.objects.get(id=id)
+	form = SenderoForm({'nombre': sendero.nombre,
+						'descripcion': sendero.descripcion,
+						'duracion': sendero.duracion,
+						'url': sendero.fotos[0].url,
+						'alt': sendero.fotos[0].alt
+						})
+	context = {
+		'form': form,
+		'sendero': sendero
+	}
+	return render(request, 'home_edit_sendero.html', context)
+
+
+def editSenderoForm(request, id):
+	context = {}
+	
+	if request.method == 'POST':
+		form = SenderoForm(request.POST)
+		if form.is_valid():
+			print("Vamos a modificar un sendero")
+			print(str(form.cleaned_data))
+			datos_formulario = form.cleaned_data
+			sendero = Sendero.objects.get(id=id)
+			sendero.nombre=datos_formulario['nombre']
+			sendero.descripcion=datos_formulario['descripcion']
+			sendero.duracion=datos_formulario['duracion']
+			sendero.save()
+			sendero.fotos = [Foto(alt=datos_formulario['alt'], url=datos_formulario['url'])]
+			sendero.save()
+			messages.add_message(request, messages.INFO, 'Sendero modificado correctamente')
+		
+	return  redirect('/senderos')
