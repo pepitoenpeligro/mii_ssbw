258d3d530448b2df8671d8b59ce4f73b7d61673c
José Antonio Córdoba Gómez <joseinn@correo.ugr.es>
2021-06-18 22:58:19 +0200

    #16 animacion en dislike y color en like y dislike

--- static/pepe/js/likes.js
+++ static/pepe/js/likes.js
@@ -1,108 +1,141 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-    
+
 
 
 
     $('.likes').css('cursor', 'pointer')
     $('.dislikes').css('cursor', 'pointer')
 
-    
 
 
-    $('.likes').click(function(){
-      console.log("subir likes a",event.target.id)
-      console.log(`/addlike/${event.target.id}`)
-      console.log("Elemento: ", $('.plikes'))
-      var elemento = event.target.id
-      //$('.likes').addClass("push")
 
-    //   
-     console.log("¿ESTA E ELEMENTO?")
+    $('.likes').click(function () {
+        console.log("subir likes a", event.target.id)
+        console.log(`/addlike/${event.target.id}`)
+        console.log("Elemento: ", $('.plikes'))
+        var elemento = event.target.id
+        //$('.likes').addClass("push")
 
-      
-      
-
-      $.ajax({
-          url: `/addlike/${event.target.id}`,
-          success: function(response){
-              var likes = JSON.parse(response["likes"]);
-              console.log(elemento, "debe tener: ", likes)
-
-              
-                
-
-
-              $('.plikes').each(function(i, obj) {
-                  console.log(i, "  ", obj)
-                  if(obj.id == elemento){
-                      obj.innerHTML=likes
-
-                      obj.animate({ 
-                        opacity: 0.4,
-                        fontSize: "6em", 
-    
-                }, 1000);
-    
-                obj.animate({ 
-                    opacity: 1,
-                    fontSize: "1em", 
-    
-                }, 1000);
-
-                  }
-              })
+        //   
+        console.log("¿ESTA E ELEMENTO?")
 
 
 
 
-              $('.likes').each(function(i, obj) {
-                console.log(i, "  ", obj)
-                if(obj.id == elemento){
+        $.ajax({
+            url: `/addlike/${event.target.id}`,
+            success: function (response) {
+                var likes = JSON.parse(response["likes"]);
+                console.log(elemento, "debe tener: ", likes)
 
-                    obj.animate({ 
-                      opacity: 0.4,
-                      fontSize: "6em", 
-        
-                    }, 1000);
-        
-                    obj.animate({ 
-                        opacity: 1,
-                        fontSize: "1em", 
-        
-                    }, 1000);
+                $('.plikes').each(function (i, obj) {
+                    console.log(i, "  ", obj)
+                    if (obj.id == elemento) {
+                        obj.innerHTML = likes
 
+                        obj.animate({
+                            opacity: 0.4,
+                            fontSize: "6em",
+                            color: 'green'
 
+                        }, 1000);
 
-                }
-            })
-          }
-      })
-      
+                        obj.animate({
+                            opacity: 1,
+                            fontSize: "1em",
+
+                        }, 1000);
+
+                    }
+                })
+
+                $('.likes').each(function (i, obj) {
+                    console.log(i, "  ", obj)
+                    if (obj.id == elemento) {
+
+                        obj.animate({
+                            opacity: 0.4,
+                            fontSize: "6em",
+
+                        }, 1000);
+
+                        obj.animate({
+                            opacity: 1,
+                            fontSize: "1em",
+
+                        }, 1000);
+                    }
+                })
+            }
+        })
+
     })
 
-    $('.dislikes').click(function(event){
-      console.log("bajar likes a ", event.target.id)
-      console.log(`/substractlike/${event.target.id}`)
-      //$('.likes').addClass("push")
+    $('.dislikes').click(function (event) {
+        console.log("bajar likes a ", event.target.id)
+        console.log(`/substractlike/${event.target.id}`)
+        //$('.likes').addClass("push")
 
-      var elemento = event.target.id
-      $.ajax({
-          url: `/substractlike/${elemento}`,
-          
-          success: function(response){
-              var likes = JSON.parse(response["likes"]);
-              console.log(likes)
+        var elemento = event.target.id
+        $.ajax({
+            url: `/substractlike/${elemento}`,
 
-              $('.plikes').each(function(i, obj) {
-                  console.log(i, "  ", obj)
-                  if(obj.id === elemento){
-                      obj.innerHTML=likes
-                      
-                  }
-              })
-          }
-      })
-      
-    })                
-  })
+            success: function (response) {
+                var likes = JSON.parse(response["likes"]);
+                console.log(likes)
+
+                $('.plikes').each(function (i, obj) {
+                    console.log(i, "  ", obj)
+                    if (obj.id === elemento) {
+                        obj.innerHTML = likes
+
+                    }
+                })
+
+                $('.plikes').each(function (i, obj) {
+                    console.log(i, "  ", obj)
+                    if (obj.id == elemento) {
+                        obj.innerHTML = likes
+
+                        obj.animate({
+                            opacity: 0.4,
+                            fontSize: "6em",
+                            color: 'red'
+
+                        }, 1000);
+
+                        obj.animate({
+                            opacity: 1,
+                            fontSize: "1em",
+                        }, 1000);
+
+                    }
+                })
+
+                $('.likes').each(function (i, obj) {
+                    console.log(i, "  ", obj)
+                    if (obj.id == elemento) {
+
+                        obj.animate({
+                            opacity: 0.4,
+                            fontSize: "6em",
+
+
+                        }, 1000);
+
+                        obj.animate({
+                            opacity: 1,
+                            fontSize: "1em",
+
+
+                        }, 1000);
+                    }
+                })
+
+
+            }
+        })
+
+    })
+})
